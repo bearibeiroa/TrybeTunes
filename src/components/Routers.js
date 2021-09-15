db2f7aaea8d8b8254baab9c64c1d649b048b65a8
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-15 14:54:16 +0100

    Adiciona Switch nas todas

--- src/components/Routers.js
+++ src/components/Routers.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Login from '../pages/Login';
-import Search from '../pages/Search';
-import Album from '../pages/Album';
-import Favorites from '../pages/Favorites';
-import Profile from '../pages/Profile';
-import ProfileEdit from '../pages/ProfileEdit';
-import NotFound from '../pages/NotFound';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import {
+  Login,
+  Search,
+  Album,
+  Favorites,
+  Profile,
+  ProfileEdit,
+  NotFound,
+} from '../pages';
 
 class Routers extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="*" component={ NotFound } />
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
         </BrowserRouter>
       </div>
     );
