f458e9489d82b83bec1faa00954b7a5cbbd2c199
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-15 21:08:16 +0100

    Requisito 5 - cria form para pesquisar artista

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { search } = this.state;
+    const SearchLength = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="search"
+              value={ search }
+              placeholder="Nome do artista"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ search.length < SearchLength }
+              onClick={ this.handleChange }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
